9a74cfd10c6dd4698380f01f3d68f931a885e7c6
Chidrup Desai <dchidrup@narolainfotech.com>
2020-05-21 10:50:12 +0530

    setup-2

---
+++ PDF_Compare_Tool/PDF_Compare_Tool/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDF_Compare_Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file1 = String.Empty;
+            string file2 = String.Empty;
+            
+            Console.WriteLine("Enter first PDF file name");
+            file1 =Convert.ToString( Console.ReadLine());
+            // Ask the user to type the second number.  
+            Console.WriteLine("Enter second PDF file name");
+            file2 =Convert.ToString( Console.ReadLine());
+           //setup
+            Console.WriteLine("First file: "+file2+" \nSecond file: "+file2+"");
+            Console.Write("\nPress any key to close the console app...");
+
+            Console.ReadKey();
+        }
+    }
+}
